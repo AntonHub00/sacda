4e405fdf597aa8e31b7562221205c78fdfde1f7c
Josue_git18 <josuevilla_hernandezqhotmail.com>
2018-12-10 00:28:04 -0600

    Parte 1/2 funcional de Estadistica/General

--- app.py
+++ app.py
@@ -481,10 +481,38 @@ def admin_students_unsubscribe():
 def admin_schedule():
     return render_template('admin/schedule.html', active = 'admin_schedule')
 
-@app.route('/administrador/estadisticas/generales')
+@app.route('/administrador/estadisticas/generales', methods = ['GET', 'POST'])
 @requires_access_level_and_session(roles['admin'])
 def admin_statistics_general():
-    return render_template('admin/statistics_general.html', active = 'admin_statistics')
+    if request.method == 'POST':
+        data = {}
+
+        data['start_date'] = request.form['start_date']
+        data['finish_date'] = request.form['finish_date']
+        data['career'] = request.form['career']
+        data['service'] = request.form['service']
+                
+        try:
+            cur.execute(f''' 
+                SELECT * FROM cita INNER JOIN estudiante ON cita.id_estudiante = estudiante.id INNER JOIN profesionista ON cita.id_profesionista = profesionista.id WHERE estudiante.carrera = (SELECT carrera.id FROM carrera WHERE carrera.descripcion = '{data['career']}') AND estudiante.genero = 'M' AND profesionista.puesto = (SELECT puesto.id FROM puesto WHERE puesto.descripcion = '{data['service']}') AND cita.fecha BETWEEN '{data['start_date']}' AND '{data['finish_date']}'
+                ''')
+            query_data = cur.fetchall()
+        except:
+            return 'Error' 
+ 
+        #mysql.connection.commit()
+        #return render_template('admin/statistics_general_view.html', active = 'admin_statistics_general', query_data = query_data)
+        return str(query_data)
+    try:
+        cur.execute(''' SELECT * FROM carrera''')
+        r_career = cur.fetchall()
+        cur.execute(''' SELECT * FROM puesto''')
+        r_service = cur.fetchall()
+    except:
+        return 'Error'
+
+
+    return render_template('admin/statistics_general.html', active = 'admin_statistics', r_service = r_service, r_career = r_career)
 
 @app.route('/administrador/estadisticas/profesionistas')
 @requires_access_level_and_session(roles['admin'])
